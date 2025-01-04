0c38aa0fe28453c34f65238e6a3d4ad76acbda2f
Supriyo Saha <mm.saha1982@gmail.com>
2025-01-05 02:45:16 +0530

    interact with abi

--- interact-with-contract/src/App.jsx
+++ interact-with-contract/src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
-import { useAccount, useConnect, useDisconnect, WagmiProvider } from "wagmi";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useReadContract,
+  WagmiProvider,
+} from "wagmi";
 import { config } from "./config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -11,10 +17,68 @@ const App = () => {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <ConnectWallet />
+        <TotalSupply />
+        <TotalBalance />
       </QueryClientProvider>
     </WagmiProvider>
   );
 };
+
+function TotalSupply() {
+  const { data, isLoading, error } = useReadContract({
+    address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    abi: [
+      {
+        constant: true,
+        inputs: [],
+        name: "totalSupply",
+        outputs: [
+          {
+            name: "",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "totalSupply",
+  });
+
+  return <div>Total supply - {JSON.stringify(data?.toString())}</div>;
+}
+
+function TotalBalance() {
+  const { data, isLoading, error } = useReadContract({
+    address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    abi: [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "balanceOf",
+    args: ["0xC22205411Ab99FaA100252163B24205a76fe8d66"],
+  });
+
+  return <div>Balance - {JSON.stringify(data?.toString())}</div>;
+}
 
 function ConnectWallet() {
   const { address } = useAccount();
